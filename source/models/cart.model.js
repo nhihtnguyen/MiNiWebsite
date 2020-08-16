bdeb3e3ac0f1c8baf5c375154a305b607234f5bc
Unknown <nguyenhoantuyetnhi@gmail.com>
2020-08-16 15:48:41 +0700

    update cart

--- source/models/cart.model.js
+++ source/models/cart.model.js
@@ -2,9 +2,8 @@ const db = require('../utils/db');
 
 module.exports = {
     all: () => db.load('select * from cart'),
-
-    singleByUserID: user_id => db.load(`select * from cart where  user_id = ${user_id}`),
-    listProductByCart: cart_id => db.load(`select * from cart_detail where  cart_id = ${cart_id}`),
-    listProductByUserID: user_id => db.load(`select* from cart as c left join cart_detail as d on c.cart_id=d.cart_id where c.customer_id =${user_id} `),
+    cartByUserId : user_id => db.load(`select cart_id from cart where customer_id= ${user_id}`),
+    listProductByCart: cart_id => db.load(`select* from cart_detail as c left join product as p on c.product_id=p.product_id where c.cart_id =${cart_id}`),
+    listProductByUser: user_id => db.load(`select d.*,p.* from cart as c left join cart_detail as d on c.cart_id=d.cart_id left join product as p on d.product_id=p.product_id where c.customer_id = ${user_id}`)
  
 };
--- source/routes/checkout.route.js
+++ source/routes/checkout.route.js
@@ -4,10 +4,67 @@ const cartModel = require('../models/cart.model');
 const router = express.Router();
 
 router.get('/cart', async(req, res) => {
-    const rows = await cartModel.listProductByUserID(1);
+    const user = req.session.authUser;
+   
+    if(user)
+    {
+       var cart =  req.session.cart;
+        if(!cart)
+        {
+    const rows = await cartModel.listProductByUser(user.user_id);
+    req.session.cart = rows;
+    cart =  req.session.cart;
+    
+        }
+    
+    var temptotal =0;
+    var promo =0;
+    for (var i =0;i<cart.length;i++)
+    {
+    cart[i].sub = cart[i].amount*cart[i].price;
+    temptotal+=cart[i].sub;
+    }
+    var total = temptotal-promo;
+
     res.render('vwCheckout/cart', {
-        products: rows,
-        empty: rows.length === 0,
+        products: cart,
+        empty: cart.length === 0,
+        total: total,
+        nopromo: promo.length ===0,
+        promo: promo,
+        temptotal: temptotal,
     });
+}
+else
+{
+res.render('vwCheckout/cart', {
+    empty: true,
+});
+}
+})
+
+router.get('/update/:product', function(req,res)
+{
+var product_id = req.params.product;
+var action = req.query.action;
+var cart = req.session.cart;
+for (var i=0;i<cart.length;i++)
+{
+    if(cart[i].product_id==product_id)
+    {
+        switch (action)
+        {
+            case "plus": cart[i].amount++;
+            break;
+            case "minus": cart[i].amount--;
+            break;
+            default:
+                console.log('update problem');
+                break;
+        }
+        break;
+    }
+}
+res.redirect('/checkout/cart');
 })
 module.exports = router;
