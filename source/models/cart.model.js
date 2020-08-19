68fae3d28833981c843a27d2091a67cbc78b32e7
Unknown <nguyenhoantuyetnhi@gmail.com>
2020-08-19 23:39:13 +0700

    add view order

--- source/models/cart.model.js
+++ source/models/cart.model.js
@@ -6,5 +6,11 @@ module.exports = {
     listProductByCart: cart_id => db.load(`select* from cart_detail as c left join product as p on c.product_id=p.product_id where c.cart_id =${cart_id}`),
     listProductByUser: user_id => db.load(`select d.*,p.* from cart as c left join cart_detail as d on c.cart_id=d.cart_id left join product as p on d.product_id=p.product_id where c.customer_id = ${user_id}`),
     add: (tableName, cart_id, product_id,subtotal) => db.load(`insert into ${tableName} values ('${cart_id}','${product_id}','1','${subtotal}') where cart_id = ${cart_id}`),
+    updateSingleItem: entity => {
+        const condition1 = { cart_id: entity.cart_id };
+        const condition2 = { product_id: entity.product_id };
+        const amount = {amount: entity.amount};
+        return db.update('cart_detail', amount, condition1, condition2);
+    },
  
 };
